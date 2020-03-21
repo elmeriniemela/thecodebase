389eafdbd9a0088785f0967819b10342740d6673
Elmeri Niemelä <niemela.elmeri@gmail.com>
2020-03-21 14:07:49 +0200

    Fix typos

--- projects/models.py
+++ projects/models.py
@@ -43,22 +43,22 @@ class Topic(models.Model):
         pre_defined = {
             'python': {
                 'sequence': 10,
-                'description': "Python is currently my goto language. "
+                'description': "Python is currently my go-to language. "
                     "It's the main language I've used professionally and in my personal projects. "
-                    "Keywords with python include Odoo-ORM, Django-framework, Flask, API Integrations and Data Analysis.",
+                    "Keywords with Python include Odoo-ORM, Django-framework, Flask, API Integrations and Data Analysis.",
             },
 
             'java': {
                 'sequence': 20,
-                'description': "During my studies Java was the teaching language at University of Helsinki. "
-                    "Using Java I learned the basic and advanced consepts of data strucutes and algorithms. "
+                'description': "During my studies at University of Helsinki, Java was the teaching language. "
+                    "Using Java I learned the basic and advanced concepts of data structures and algorithms. "
                     "I believe strongly that the theoretical understanding provides important tools when analysing practical problems.",
             },
 
             'ansible': {
                 'sequence': 30,
-                'description': "Automating processes and making life easier has always been my passion. "
-                    "Since both Ansible and Odoo ERP are open source and based on python, "
+                'description': "Automating processes and making life easier have always been my passion. "
+                    "Since both Ansible and Odoo ERP are open source and based on Python, "
                     "I integrated them at SprintIT and created an interface to handle automated customer deployments.",
             },
 
@@ -66,8 +66,8 @@ class Topic(models.Model):
                 'title': 'C/C++',
                 'sequence': 40,
                 'description': "I've studied the basics of C/C++. "
-                    "There's alot I'd like to do with C++ for example rewrite some of the games I've made with pygame. "
-                    "What facinates me about C in particular is it's closeness to what the actual hardware supports.",
+                    "There's alot I'd like to do with C++: for example rewrite some of the games I've made with pygame. "
+                    "What facinates me about C in particular is its closeness to what the actual hardware supports.",
             },
 
             'javascript': {
@@ -80,9 +80,9 @@ class Topic(models.Model):
             'web-development': {
                 'title': 'Web Devopment',
                 'sequence': 60,
-                'description': "This website is one of the examples of my skills with web based technologies. "
+                'description': "This website is one of the examples of my skills with web-based technologies. "
                     "Currently I'm interested to learn new web technologies such as Angular or React for frontend and Node.js and MongoDB for backend. "
-                    "Automating and deploying Cloud based systems is a big part of my job at SprintIT",
+                    "Automating and deploying Cloud-based systems is a big part of my job at SprintIT",
 
             },
         }
